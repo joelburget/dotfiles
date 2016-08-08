dfa1ac2a1cd3e21b3c06ca0293448ffa686e7646
Joel Burget <joelburget@gmail.com>
2016-08-08 17:36:38 -0400

    Add spacemacs.

---
+++ spacemacs/.emacs.d/private/writeroom/packages.el
@@ -0,0 +1,19 @@
+;;; packages.el --- Writeroom Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015 fmdkdd
+;;
+;; Author: fmdkdd
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(setq writeroom-packages
+  '(writeroom-mode))
+
+(defun writeroom/init-writeroom-mode ()
+  (use-package writeroom-mode
+    :init
+    (evil-leader/set-key "aw" 'writeroom-mode)))
